5ebaad521746edb0d42ac0c1bf8fcdab09591ab8
dvm <dmpersonalemail@gmail.com>
2024-04-07 11:00:57 -0400

    iterating on startup script for webdrivers

--- tests/setup/utils/start.py
+++ tests/setup/utils/start.py
@@ -1,4 +1,5 @@
 import subprocess
+import logging
 from yaml import load, dump
 
 try:
@@ -6,5 +7,21 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-stream = open('config.yaml', 'r')
-print(load(stream, Loader=Loader))
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='startup.log', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
+configToOpen = open('config.yaml', 'r')
+configAsDictionary = load(configToOpen, Loader=Loader)
+logging.debug(configAsDictionary)
+
+gecko = ''
+chrome = ''
+for name, path in configAsDictionary.items():
+    if path == 'CHROME_PATH':
+        chrome = path + './chromedriver'
+    if path == 'GECKO_PATH':
+        gecko = path + './geckodriver'
+
+drivers = [subprocess.Popen(program) for program in [chrome, gecko]]
+for driver in drivers:
+    driver.wait()
