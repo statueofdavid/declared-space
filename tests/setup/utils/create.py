86db8624e44186dca702f6c8e9c9019c3746ff48
dvm <dmpersonalemail@gmail.com>
2024-04-09 09:58:49 -0400

    continued troubleshooting request logic

--- tests/setup/utils/create.py
+++ tests/setup/utils/create.py
@@ -13,14 +13,25 @@ logging.basicConfig(filename='create.log', encoding='utf-8', format='%(asctime)s
 
 def response_parser(response):
     logging.debug(response) 
-
-    for name, value in response.json():
-        match name:
-            case 'sessionId':
-                logging.debug('sessionId found: ' + value)
-                return value
-            case _:
-                logging.debug('not the id')
+    
+    # research adding context to debug logging to reveal browser opinions
+    match response:
+        case str(response) if '405' in response:
+            logging.debug('Bad Method, what about another tool like curl')
+            # when talking to gecko, bad url path throws a 405
+            return 'Bad Method'
+        case str(response) if '404' in response:
+            logging.debug('unknown command, what about another tool like curl')
+            # when talking to chrome, bad url path throws a 404
+            return 'Unknown Command' 
+        case _:
+            for name, value in response.json().items():
+                match name:
+                    case 'sessionId':
+                        logging.debug('sessionId found: ' + value)
+                        return value
+                    case _:
+                        logging.debug('not the id')
     
     return 'not found'
 
@@ -47,11 +58,14 @@ gecko = localhost + ':4444'
 
 for file in files:
     if file.find('chrome'):
-        response = requests.post(chrome, file)
+        logging.debug('server path: ' + chrome + ', file path: ' + session + file)
+        response = requests.post(chrome, session + file)
+        logging.debug(response)
         response_parser(response)
 
-    if file.find('gecko'):
-        response = requests.post(gecko, file)
-        response_parser(response)
+#    if file.find('gecko'):
+#        logging.debug('server path: ' + gecko + ', file path: ' + session + file)
+#        response = requests.post(gecko, session + file)
+#        response_parser(response)
 
 
