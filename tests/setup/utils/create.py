8cc9dc92c8f2ddadb5ab671b53931d5ae0c807c9
dvm <dmpersonalemail@gmail.com>
2024-04-13 01:25:04 -0400

    cleaned up create.py, able to get session id from response

--- tests/setup/utils/create.py
+++ tests/setup/utils/create.py
@@ -8,27 +8,8 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-def get_new_session_id(response):
-    logging.debug(response) 
-    
-    # research adding context to debug logging to reveal browser opinions
-    match response:
-        case str(response) if '200' in response:
-            body = response.text
-            logging.debug('Ok, is there a body: ' + body)
-            return 'Ok'
-        case str(response) if '405' in response:
-            logging.debug('Bad Method, what about another tool like curl')
-            # when talking to gecko, bad url path throws a 405
-            return 'Bad Method'
-        case response if '404' in response:
-            logging.debug('unknown command, what about another tool like curl')
-            # when talking to chrome, bad url path throws a 404
-            return 'Unknown Command' 
-        case _:
-            logging.debug('unhandled response, check the logs')
-
-    return 'not found'
+def get_session_id(response):
+    return response.json()["value"]["sessionId"]
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='create.log', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
@@ -38,21 +19,24 @@ configAsDictionary = load(configToOpen, Loader=Loader)
 logging.debug(configAsDictionary)
 
 session = ''
+chrome = ''
+gecko = ''
 for name, configValue in configAsDictionary.items():
     if name == 'SESSION_PATH':
         session = configValue
         logging.debug('session path: ' + session)
+    if name == "CHROME_SOCKET":
+        chrome = configValue
+        logging.debug('chrome socket: ' + chrome)
+    if name == "GECKO_SOCKET":
+        gecko = configValue
+        logging.debug('gecko socket: ' + gecko)
 
 files = os.listdir(session)
 logging.debug(files)
 
-
 if not files:
     logging.warn('session path: ' + session + ', is empty: ' + files)
-
-localhost = 'http://localhost'
-chrome = localhost + ':9515'
-gecko = localhost + ':4444'
 
 for file in files:
     with open(session + file, 'r') as payload:
@@ -65,7 +49,8 @@ for file in files:
         session_response = requests.post(chrome + '/session', headers=headers, data=body)
         logging.debug(session_response)
         
-        get_new_session_id(session_response)
+        session_id = get_session_id(session_response)
+        logging.debug('Created, id: ' + session_id)
 
 # UNTESTED because my firefox installation is broken cause I didn't thoughtfully update my desktop environment
 #    if file.find('gecko'):
