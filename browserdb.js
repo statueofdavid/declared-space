e398d4f59eb60742ef58e5bb48d85efec8ac58ae
dvm <dmpersonalemail@gmail.com>
2024-04-28 03:05:03 -0400

    registering users in idb

--- browserdb.js
+++ browserdb.js
@@ -1,32 +1,71 @@
+/*
+ *******************************************************************
+ * Thank you mozilla.org
+ * REF: https://developer.mozilla.org/en-US/docs/Web/API/IDBDatabase
+ *
+ * although I do wish you had some better error handling in the ex.
+ ********************************************************************
+ */
 
-
-const dbName = "dsLocal"
+const dbName = "dsLocalTest"
 const dbVersion = 3;
 const objStore = "dsRegistered"
 
-const request = window.indexedDB.open(dbName, dbVersion);
+console.log("constants: " + dbName + ", " + dbVersion + ", "+ objStore);
 
-request.onerror = (event) => {
-  // Do something with request.errorCode!
-  console.log(request.errorCode);
-};
-request.onsuccess = (event) => {
-	const db = event.target.result;
-	console.log(request.result);
+async function open() {
+	const request = window.indexedDB.open(dbName, dbVersion);
+	return new Promise((resolve, reject) => {	
+		request.onsuccess = (event) => resolve(event.target.result);
+		request.onerror = (event) => reject(new Error("Rip: " + request.errorCode));
 
-	const email = document.getElementById("email");
-	const password = document.getElementById("password");
-	const repeatpw = document.getElementById("confirmPassword");
+		request.onupgradeneeded = (event) => {
+			const db = event.target.result;
+			const store = db.createObjectStore(objStore, {keyPath: "email",});
+			console.log("ObjectStore: ", store);
+
+			store.createIndex("emailIndex", "email", {unique: true});
+			store.createIndex("passwordIndex","",{unique: false});
+			store.createIndex("tosAgreementIndex", "tosAgreement", {unique: false});
+		};
+	});
+}	
+
+async function addUser(db, email, password, tosAgree) {
 
 	const data = {
 		email: email,
 		password: password,
+		tosAgreement: tosAgree,
 	};
 
-	const transaction = db.transaction(objStore, 'readWrite');
+	console.log(data);
+
+	const transaction = db.transaction(objStore, 'readwrite');
+	console.log("starting transaction log...");
+		
 	const persist = transaction.objectStore(objStore);
-	
 	persist.add(data);
-};
+}
 
 
+async function registerUser() {
+
+        var element = document.getElementById("signUpInputEmail");
+	var passwordElement = document.getElementById("signUpInputPassword");
+        var tosToggleElement = document.getElementById("invalidCheck");
+        
+	var email = element.value;
+        var password = passwordElement.value;
+        var tosToggle = tosToggleElement.checked;
+
+	try {
+		const db = await open();
+		console.log("love this db opening journey for us");
+
+		await addUser(db, email, password, tosToggle);
+		console.log("you are registered with deez");
+	} catch (error) {
+		console.error("crap: ", error);
+	}
+}
